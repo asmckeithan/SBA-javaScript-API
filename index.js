8c3c1d509878dfa204c18a580a865521bbde9ae9
asmckeithan <annesha.mckeithan@gmail.com>
2024-05-20 22:06:15 -0400

    added Audio

--- index.js
+++ index.js
@@ -21,44 +21,24 @@ async function dogAPI() {
 }
 
 dogAPI();
+// fetch('https://jsonplaceholder.typicode.com/posts', {
+//   method: 'POST',
+//   body: JSON.stringify({
+//     title: 'foo',
+//     body: 'bar',
+//     userId: 1,
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((json) => () {
+// console.log(json)
+//   });
 
-
-
-// url = `https://api.thedogapi.com/v1/images/search?limit=10`;
-
-// fetch(url)
-//     .then(function(response){
-//         return response.json();
-//      })
-//     .then(function(data){
-//     console.log(data)
-//     })
-// }
-// getRandomDog(_url)
-
-
-// const api_key = ("live_yHGf5z1JXE3ehtAnbb11DXwhO4VCbam5F8bsz1lIUZbT2doKGu03CqPFJm6pkc10");
-
-// a variable to store the information about the breeds
-
-// let storedBreeds = [];
-
-// // a function to select a random breed
-// function showCatImageAndInformation(index) {
-
-//     // This will display the image of the cat
-//       document.getElementById("cat_image").src = storedBreeds[index].image.url;
-    
-//     // This will get the breed name
-//       document.getElementById("breed_name").innerHTML = storedBreeds[index].name;
-    
-//     // This will get the wiki link
-//       document.getElementById("wiki_link").href = storedBreeds[index].wikipedia_url;
-    
-//       document.getElementById("wiki_link").innerHTML =
-//         storedBreeds[index].wikipedia_url;  
-    
-//     // This will get the characteristics of the cat
-//       document.getElementById("breed_json").textContent =
-//         storedBreeds[index].temperament;
-//     }
+//   const dogBreed = document.getElementById('type')
+//   const inputdogBreed = data.title
+let myAudio = document.querySelector('#audio')
+myAudio.play()
+  
