496a15eb170bfedd87c9fe6e83c12f95d37e054b
asmckeithan <annesha.mckeithan@gmail.com>
2024-05-21 22:09:41 -0400

    Import added

---
+++ form.js
@@ -0,0 +1,28 @@
+
+//First we find the form 
+const form = document.getElementById('dogInfo');
+
+//NExt we add an event Listner to the form 
+const formEvent = function () {
+form.addEventListener('submit',(info) => {
+    //Next we prevent the default actions and configure our own
+    info.preventDefault();
+    //making our own configurations 
+    const dogInfo = new FormData(form)
+//create an object for javascript 
+const data = Object.fromEntries(dogInfo)
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  headers: {
+    'Content-type': 'application/data'
+  },
+  body: JSON.stringify(data)
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+//   .catch(error => console.log(error));
+})
+}
+
+export default formEvent;
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-//Seperate each section and import data from new files 
+//Audio input of Window
+import '/form.js'
+
+let myAudio = document.querySelector('#audio')
+document.body.appendChild(myAudio);
+myAudio.play();
+
 
 //API fetch 
 //first find the element to attach API to
 const button = document.getElementById('btn')
  button.addEventListener("click", dogAPI);
-
-const apiKey = ("live_yHGf5z1JXE3ehtAnbb11DXwhO4VCbam5F8bsz1lIUZbT2doKGu03CqPFJm6pkc10")
 
 async function dogAPI() {
     dogEl.style.cursor = 'wait';
@@ -23,37 +27,4 @@ async function dogAPI() {
 }
 
 dogAPI();
-
-// const type = document.getElementById('type')
-
-
-//--------------------ERROR-------------------
-// unable to push input into JSON format . Please advise 
-// fetch('https://jsonplaceholder.typicode.com/posts')
-//   .then((response) => response.json())
-//   .then((json) => console.log(json));
-  
-// const ajax = async (config) => {
-//     const request = await fetch('https://jsonplaceholder.typicode.com/post', {
-//         method: 'post',
-//         headers: {
-//             'Accept': 'application/json',
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(config.ajax)
-//     });
-//     res = await request.json();
-//     console.log('response', res)
-//     return response
-// }
-
-// // usage
-// response = ajax({
-//     method: 'POST',
-//     url: 'example.com',
-//     payload: {"name": "Stackoverflow"}
-// })
-
-
-let myAudio = document.querySelector('#audio')
-myAudio.play()
+//Form
